ff11e87e4550f572073d40af616be967c08f01eb
erica-w-fu <72369224+erica-w-fu@users.noreply.github.com>
2022-09-22 07:52:52 -0700

    Update QueryProcessor.java

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -3,11 +3,14 @@ package com.develogical;
 public class QueryProcessor {
 
     public String process(String query) {
-        if (query.toLowerCase().contains("shakespeare")) {
-            return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
-                    "English poet, playwright, and actor, widely regarded as the greatest " +
-                    "writer in the English language and the world's pre-eminent dramatist.";
-        }
-        return "";
+        Jwiki jwiki = new Jwiki(query.tolowerCase());
+        return jwiki.getExtractText()
+        
+//         if (query.toLowerCase().contains("shakespeare")) {
+//             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
+//                     "English poet, playwright, and actor, widely regarded as the greatest " +
+//                     "writer in the English language and the world's pre-eminent dramatist.";
+//         }
+//         return "";
     }
 }
